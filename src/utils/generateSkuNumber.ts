b63a3a16d6b9744ccd673aa7fe3d4972f6c999a7
Ben Halverson <benhalverson33@gmail.com>
2024-12-01 17:47:22 -0800

    added tests for util functions

--- src/utils/generateSkuNumber.ts
+++ src/utils/generateSkuNumber.ts
@@ -1,6 +1,20 @@
-export const generateSkuNumber = (productName: string, color: string, ) => {
-	const productCode = productName.slice(0, 8).toUpperCase();
-	const colorCode = color.slice(0, 5).toUpperCase();
+/**
+ * Generate a SKU number based on the product name and color
+ * @param productName - The name of the product
+ * @param color - The color of the product
+ * @returns Then generated SKU number
+ */
+export const generateSkuNumber = (productName: string, color: string) => {
+	if (!productName.trim()) {
+		throw new Error('Product name cannot be empty');
+	}
+
+	if(!color.trim()) {
+		throw new Error('Color cannot be empty');
+	}
+
+	const productCode = productName.replace(/\s+/g, '').slice(0, 8).toUpperCase();
+	const colorCode = color.replace('#', '').substring(0, 5).toUpperCase();
 
 	return `${productCode}-${colorCode}`;
 };
---
+++ test/utils/generateSkuNumber.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest';
+import { generateSkuNumber } from '../../src/utils/generateSkuNumber';
+
+describe('Generate SKU number', () => {
+	it('should generate a SKU number', () => {
+		const productName = 'test product';
+		const productName2 = 'F1 Chassis';
+		const productName3 = 'White T-Shirt';
+		const productName4 = ' a t4 a - 4 a # $';
+		const blackColor = '#000000';
+		const whiteColor = '#FFFFFF';
+
+		const skuNumber = generateSkuNumber(productName, blackColor);
+		const skuNumber2 = generateSkuNumber(productName2, blackColor);
+		const skuNumber3 = generateSkuNumber(productName3, whiteColor);
+		const skuNumber4 = generateSkuNumber(productName4, whiteColor);
+		expect(skuNumber).toEqual('TESTPROD-00000');
+		expect(skuNumber2).toEqual('F1CHASSI-00000');
+		expect(skuNumber3).toEqual('WHITET-S-FFFFF');
+		expect(skuNumber4).toEqual('AT4A-4A#-FFFFF');
+	});
+
+	it('should handle empty product name', () => {
+		const productName = '';
+		const blackColor = '#000000';
+		expect(() => generateSkuNumber(productName, blackColor)).toThrowError('Product name cannot be empty');
+	});
+
+	it('should handle empty product color', () => {
+		const productName = 'test product';
+		const blackColor = '';
+		expect(() => generateSkuNumber(productName, blackColor)).toThrowError('Color cannot be empty');
+	});
+
+
+});
