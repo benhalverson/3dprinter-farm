b86be5b56495bf234b477d3944d584b96b4fd688
Ben Halverson <benhalverson33@gmail.com>
2025-04-19 17:22:17 -0700

    fixed orderNumber generation

--- src/utils/generateOrderNumber.ts
+++ src/utils/generateOrderNumber.ts
@@ -1,5 +1,6 @@
 export const generateOrderNumber = () => {
-	const datePrefix = new Date().toISOString().replace(/[-:.TZ]/g, '').slice(0, 10);
-	const randomNumber = Math.floor(100000 + Math.random() * 900000);
+	const date = new Date();
+	const datePrefix = date.toISOString().slice(0, 10).replace(/-/g, ''); // 'YYYYMMDD'
+	const randomNumber = Math.floor(100000 + Math.random() * 900000); // 6 digits
 	return `${datePrefix}-${randomNumber}`;
 };
--- test/utils/generateOrderNumber.spec.ts
+++ test/utils/generateOrderNumber.spec.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { generateOrderNumber } from '../../src/utils/generateOrderNumber';
 
 describe('Generate order number', () => {
-	it.skip('should generate a unique order number', async () => {
+	it('should generate a unique order number', async () => {
 		const dateString = '2024-12-01T22:43:03.169Z'
 		const orderNumber = generateOrderNumber();
 		expect(orderNumber).toMatch(/^\d{8}-\d{6}$/);
