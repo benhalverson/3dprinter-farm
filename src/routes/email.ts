e18a0e4527d68fd65e73c185ce43693d24534bf6
Ben Halverson <benhalverson33@gmail.com>
2025-05-15 13:12:35 -0700

    dont allow duplicate emails

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -1,10 +1,31 @@
 import { zValidator } from '@hono/zod-validator';
 import factory from '../factory';
 import { leads, leadsSchema } from '../db/schema';
+import { eq } from 'drizzle-orm';
 
 const email = factory
 	.createApp()
 	.post('/email', zValidator('json', leadsSchema), async (c) => {
+		try {
+			// check if the email is already in the database
+			const { email } = c.req.valid('json');
+			const existing = await c.var.db
+				.select()
+				.from(leads)
+				.where(eq(leads.email, email))
+				.get();
+
+			if (existing) {
+				return c.json(
+					{ status: 'error', message: 'Email is already subscribed.' },
+					400
+				);
+			}
+		} catch (error) {
+			console.error('Error checking email:', error);
+			return c.json({ status: 'error', message: 'Internal Server Error' }, 500);
+		}
+
 		try {
 			const auth = `${c.env.MAILJET_API_KEY}:${c.env.MAILJET_API_SECRET}`;
 			const base64Auth = Buffer.from(auth).toString('base64');
@@ -45,6 +66,23 @@ const email = factory
 			console.error('Error:', error);
 			return c.json({ status: 'error', message: 'Internal Server Erro' }, 500);
 		}
+	})
+	.post('/email/confirm', async (c) => {
+		try {
+			const payload = await c.req.json();
+			if (!email || payload.email !== 'subscribe') {
+				return c.json({ status: 'error', message: 'Invalid request' }, 400);
+			}
+
+			await c.var.db
+				.update(leads)
+				.set({
+					status: 'confirmed',
+					confirmedAt: Date.now(),
+					updatedAt: Date.now(),
+				})
+				.where(eq(leads.email, payload.email));
+		} catch (error) {}
 	});
 
 export default email;
